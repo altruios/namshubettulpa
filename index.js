c323cee0f33cce5834d03e5643230676cd1092e2
alturios <paul.kosmala@gmail.com>
2023-05-01 21:34:21 -0700

    process port default

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT ||3000
 const path = require('path');
 const {readFile, readFileSync } = require('fs')
 const {EOL} = require('os');
