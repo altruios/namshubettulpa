3fff17b80bd7a46ee2f13cb3913dd965862508c5
alturios <paul.kosmala@gmail.com>
2023-05-09 23:33:32 -0700

    mdx-with classes

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 const port = process.env.PORT ||3000
 const path = require('path');
 const {readFile, readFileSync } = require('fs')
-const {EOL} = require('os');
-const mdx_parser = require("./mdx_parser");
+const transform_to_md = require("./mdx_parser");
 
 //console.log(mdx_parser,"is parser")
 const BASE_DIR = path.join(__dirname, 'public');;
@@ -12,110 +11,9 @@ let cwd = BASE_DIR;
 app.set('public', path.join(__dirname, 'public'));
 
 
-const bg_colors=["#248bc5","#b5b5ba","#af80af","#b58782","#6f6f6f"] //blue // grey // red / greyred //dg
-const fg_colors=["#fafafa","#000070"] //magenta//green
-
-async function transform_to_md(cwd,callback){
-    var re = /\[(.*?)\]/g
-    var re2 = /\((.*?)\)/g
-    var re3 = /\{(.*?)\}/g
-    return  readFile(cwd,"utf-8",(err,data)=>{
-        if(data==undefined||err){
-            console.log("got this - ",data,err,cwd);
-            callback("<h1>there is no file here</h1>",null);
-            return;
-        }
-        const links = data?.match(/[^!](\[(.*?)\]\(.*?\))/g) ||[]
-        const images = data?.match(/[!](\[(.*?)\]\(.*?\)\{?.*?\})/g) ||[]
-        const headers = data?.match(/((?<atxlayer>#+)\s*(?<atxname>.+))|((?<setexname>[\w|\d|\s|-]+)\n(?<setexLayer>[-|=]{2,}))/g)||[]; // matches lines with # at start
-
-
-        headers.forEach(head=>{
-            const count = head.match(/^#+/)[0].length;
-            const htext = head.replace(/^#+/,"");
-            const replace = `<h${count} style="text-align: center;">${htext}</h${count}>`
-            data=data.replace(head,replace);
-        })
-
-        links.forEach(link=>{
-            const m1 = link.match(re2)[0].replace("(","").replace(")","");
-            const m2 = link.match(re)[0].replace("]","").replace("[","");
-            const replace = `<a href="${m1}">${m2}</a>`
-            data=data.replace(link,replace)
-        });
-        images.forEach(img=>{
-            
-            if(cwd.includes("/public/index.md")){
-
-            }
-            const urlandhover = img.match(re2)[0].replace("(","").replace(")","");
-            const style = img.match(re3)[0]?.replace("{","")?.replace("}","");
-            const url = urlandhover.slice(0,urlandhover.indexOf(" "))
-            const altText = img.match(re)[0].replace("]","").replace("[","");
-            
-            const replace = `<img src="${url}" alt="${altText}" style="${style}"/>`
-            data=data.replace(img,replace)
-        })
-        const script_bypass={data:""}
-        const script = data.match(/<script[^>]*>[\s\S]*?<\/script>/gim)
-        script_bypass.data=script?script[0]:"";``
-        data=data.replaceAll(script,"");
-        const blocks =mdx_parser(data)
-        if(blocks){
-
-            const start=data.indexOf("{");
-            const end = data.lastIndexOf("}");
-            const start_str = data.slice(0,start);
-            const end_str = data.slice(end+1);
-            data= start_str+blocks+end_str
-        }else{
-            const lines = data.split(EOL).filter(x=>x!=''&&x[0]!="{"&& !(x[1]=='h'&&x[0]=="<"));
-        
-            lines.forEach((line,i,arr)=>{
-                if('123456789'.includes(line[0])){
-                    const stripped_line = line.replace(line.match(/[0-9]+\.\s/gm),"")
-                    const data_line = stripped_line.slice(stripped_line.indexOf(".")+1);
-                    if(line[0]=="1"&&line[1]=="."){
-
-                        data=data.replace(line,`<ol style="font-size:5vh; margin-left:2vw" ><li >${data_line}</li>`)
-                    }
-                    else if(!arr[i+1] || !'123456789'.includes(arr[i+1][0])){
-                        data=data.replace(line,`<li>${data_line}</li></ol>`)
-                    }else{
-                    data=data.replace(line,`<li>${data_line}</li>`)
-                    }
-                }else{
-
-                    data=data.replace(line, `<p style="font-size:5vh;">${line}</p>`);
-                }
-            })
-        }
-        data=`<html><meta charset="UTF-8"><meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      />
-      <title>Namshub Et'Tulpa</title>
-      <style>
-
-        .cursor {
-            animation: 2s linear infinite b;
-          }
-          
-          @keyframes b {
-            0% {
-              visibility: hidden;
-            }
-            50% {
-              visibility: hidden;
-            }
-            100% {
-              visibility: visible;
-            }
-          }</style><body style="background-color:${bg_colors[4]}; ${blocks? `font-size:3vh;display:flex;flex-flow:column nowrap;"`:''} >${data}</body>${script_bypass.data}</html>`
-        data=data.replace(/([\<][\p][\>][\<][\/][\p][\>])/gm, "")
-        callback(null,data);
-    })        
-}
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "style.css");
+  });
 app.get("/",(req,res)=>{
     const start_path = path.join(BASE_DIR,"index.html")
     readFile(start_path,"utf-8",(err,data)=>{
@@ -124,8 +22,9 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/*",(req,res)=>{
-
     const file_request = req.params[0];
+    if(file_request.includes(".css")) return res.sendFile(path.join(BASE_DIR,'style.css'))
+    
     const is_html_file = file_request.includes(".html")
     const is_md_file =file_request.includes(".md");
     const is_short_story = file_request.includes("shorts")
@@ -155,7 +54,7 @@ app.get("/*",(req,res)=>{
         data=`<html><title>Namshub Et'Tulpa</title><meta charset="UTF-8"><meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
-      /><body style="background-color:${bg_colors[4]};white-space: pre-wrap;" ><p>${data}</p></body>${script_bypass.data}</html>`
+      /><body style="white-space: pre-wrap;" ><p>${data}</p></body>${script_bypass.data}</html>`
 
         res.send(data)
     }):transform_to_md(cwd,(err,r)=>err?res.send(err):res.send(r))
@@ -166,4 +65,3 @@ function escapeHtml  (unsafe) {
 app.listen(port, () => {
     console.log("listening",port)
 })
-
--- mdx_parser.js
+++ mdx_parser.js
@@ -1,14 +1,14 @@
-const {readFile, readFileSync } = require('fs')
+const {readFile} = require('fs')
 const {EOL} = require('os');
-
 const speakers =require('./speakermap.js');
 const emoji_map = require('./emoji_map.js');
+
 const defaultnarrator = {current:speakers.M}
 const mdx_parser=(text)=>{
     const data = get_mdx_data(text);
     if(data.length<100) return null;
-    const blocks = seprate_blocks(data);
-    let blockstr=`<block style="background-color:inherit; width:97vw;display:flex;flex-flow:column nowrap;align-items:start;margin:auto 0 auto 0; max-width:1170;align-self:center;">`
+    const blocks = separate_blocks(data);
+    let blockstr=`<block class="block">`
     for(let i=0;i<blocks.length;i++){
         blockstr+=mdx_block_parser(blocks[i],defaultnarrator)
     }
@@ -21,82 +21,7 @@ const get_mdx_data=(text)=>{
     const block = text.slice(blockstart,blockend+1);
     return block;
 }
-async function transform_to_md(cwd,callback){
-    var re = /\[(.*?)\]/g
-    var re2 = /\((.*?)\)/g
-    var re3 = /\{(.*?)\}/g
-    return  readFile(cwd,"utf-8",(err,data)=>{
-        if(data==undefined||err){
-            callback("<h1>there is no file here</h1>",null);
-            return;
-        }
-        const links = data?.match(/[^!](\[(.*?)\]\(.*?\))/g) ||[]
-        const images = data?.match(/[!](\[(.*?)\]\(.*?\)\{?.*?\})/g) ||[]
-        const headers = data?.match(/((?<atxlayer>#+)\s*(?<atxname>.+))|((?<setexname>[\w|\d|\s|-]+)\n(?<setexLayer>[-|=]{2,}))/g)||[]; // matches lines with # at start
-
-
-        headers.forEach(head=>{
-            const count = head.match(/^#+/)[0].length;
-            const htext = head.replace(/^#+/,"");
-            const replace = `<h${count} style="text-align: center;">${htext}</h${count}>`
-            data=data.replace(head,replace);
-        })
-
-        links.forEach(link=>{
-            const m1 = link.match(re2)[0].replace("(","").replace(")","");
-            const m2 = link.match(re)[0].replace("]","").replace("[","");
-            const replace = `<a href="${m1}">${m2}</a>`
-            data=data.replace(link,replace)
-        });
-        images.forEach(img=>{
-            
-            if(cwd.includes("/public/index.md")){
-
-            }
-            const urlandhover = img.match(re2)[0].replace("(","").replace(")","");
-            const style = img.match(re3)[0]?.replace("{","")?.replace("}","");
-            const url = urlandhover.slice(0,urlandhover.indexOf(" "))
-            const altText = img.match(re)[0].replace("]","").replace("[","");
-            
-            const replace = `<img src="${url}" alt="${altText}" style="${style}"/>`
-            data=data.replace(img,replace)
-        })
-        const blocks =mdx_parser(data)
-        if(blocks){
-
-            const start=data.indexOf("{");
-            const end = data.lastIndexOf("}");
-            const start_str = data.slice(0,start);
-            const end_str = data.slice(end+1);
-            data= start_str+blocks+end_str
-        }else{
-            const lines = data.split(EOL).filter(x=>x!=''&&x[0]!="{"&& !(x[1]=='h'&&x[0]=="<"));
-        
-            lines.forEach((line,i,arr)=>{
-                if('123456789'.includes(line[0])){
-                    const stripped_line = line.replace(line.match(/[0-9]+\.\s/gm),"")
-                    const data_line = stripped_line.slice(stripped_line.indexOf(".")+1);
-                    if(line[0]=="1"&&line[1]=="."){
-
-                        data=data.replace(line,`<ol style="font-size:5vh; margin-left:2vw" ><li >${data_line}</li>`)
-                    }
-                    else if(!arr[i+1] || !'123456789'.includes(arr[i+1][0])){
-                        data=data.replace(line,`<li>${data_line}</li></ol>`)
-                    }else{
-                    data=data.replace(line,`<li>${data_line}</li>`)
-                    }
-                }else{
-
-                    data=data.replace(line, `<p style="font-size:5vh;">${line}</p>`);
-                }
-            })
-        }
-        data=`<html><meta charset="UTF-8"><body style="background-color:${bg_colors[4]}; ${blocks? `font-size:3vh;display:flex;flex-flow:column nowrap;"`:''} >${data}</body></html>`
-        data=data.replace(/([\<][\p][\>][\<][\/][\p][\>])/gm, "")
-        callback(null,data);
-    })        
-}
-const seprate_blocks=(data)=>{
+const separate_blocks=(data)=>{
     let token_maker = "";
     const delimiters="\"\'\~\`\*";
     const delimiters_stack =[];
@@ -146,22 +71,12 @@ const mdx_block_parser = (block,dfn)=>{
     let ref=block;
     const regex_settings = ref.match(/^[m][n][:](.*)/gm)
     block = block.replace(regex_settings,"")
-    let settings=dfn.current;
     if(regex_settings!=null &&regex_settings.length!=0){
         const key=regex_settings[0].slice(regex_settings[0].indexOf(":")+1);
         const current = get_speaker_with_title(key);
         dfn.current=current;
-        settings=current;
     }
 
-
-//escaped sequences
-
-
-
-
-
-//main story
     const pw = ref.match(/\\{0}\`([^`]*)\`/gm)||[];
     pw.forEach(r=>ref=ref.replace(r,""))
     const aw = ref.match(/\\{0}\*([^*]*)\*/gm)||[];
@@ -173,76 +88,6 @@ const mdx_block_parser = (block,dfn)=>{
     const tw = ref.match(/\\{0}\'([^']*)\'/gm)||[];
     tw.forEach(r=>ref=ref.replace(r,""));
 
-
-
-
-    const scp=(txt,internal_data_flag)=>{
-        //console.log(txt,"is raw text")
-        const text_div =(txt,mainNB,speaker,colors)=> `
-            <div style="${mainNB?`text-align:left; margin: 0 auto 0 0;`:
-                `text-align:right; margin: 0 0 0 auto;`} background-color:${colors[1]};
-                border:solid;padding:3; border-radius:35px; width:fit-content;min-width:20vw !important;max-width:85vw;  ">
-                <div style="font-size:2.5vh; padding-left:25;padding-right:25;">${speaker}</div>
-                <div style="padding-${mainNB?'left':'right'}: 15; color:${colors[0]}">${txt}</div>
-                </div>`
-        const speaker_regex = txt.match(/\w+[:][:]/gm);
-        const emoji_regex = txt.match(/[$][E][\w]*[:]/gm);
-        if(emoji_regex){
-          //  console.log(emoji_regex);
-            for(const match of emoji_regex){
-            let key = match.slice(2,emoji_regex.indexOf(":"));
-            let emoji = emoji_map[key];
-           // console.log(key,emoji);
-           // console.log(txt);
-            txt=txt.replace(match, emoji);
-           // console.log(txt);
-            }
-        }
-
-
-        const bg_colors=["#248bc5","#b5b5ba","#af80af","#b58782"] //blue // grey // red / greyred
-        const fg_colors=["#fafafa","#000070"] //magenta//green
-        switch(txt[0]){
-            case "\"": 
-                if(internal_data_flag){
-                    return `<span style="background-color:${bg_colors[0]}; color:${fg_colors[0]}">${txt}</span>`;
-                }
-                if(speaker_regex){
-                    let key = speaker_regex[0].slice(0,speaker_regex.indexOf(":")-1);
-                    //console.log("key is ",key,speaker_regex)
-                    const speaker = get_speaker_with_title(key);
-                    const mainFlag = speaker== defaultnarrator.current;
-                    const dialouge = txt[0]+txt.slice(txt.indexOf("::")+2)
-                    const d = `<span style="background-color:inherit;">${dialouge}</span>`
-                    const colors = mainFlag? [fg_colors[0],bg_colors[0]]:[fg_colors[1],bg_colors[1]];
-                    return text_div(d,mainFlag,speaker,colors);
-                }
-                return `<span style="background-color:inherit;">${txt}</span>`;
-            case "\'": 
-            if(internal_data_flag){
-                return `<span style="background-color:${bg_colors[2]}; color:${fg_colors[1]}">${txt}</span>`;
-            }
-            if(speaker_regex){
-                let key = speaker_regex[0].slice(0,speaker_regex.indexOf(":")-1);
-                    const speaker = get_speaker_with_title(key);
-                    const mainFlag = speaker== defaultnarrator.current;
-                    const dialouge = txt[0]+txt.slice(txt.indexOf("::")+2)
-                    const colors = mainFlag? [fg_colors[1],bg_colors[2]]:[fg_colors[1],bg_colors[3]];
-                    const d = `<span style="background-color:inherit;">${dialouge}</span>`
-                    return text_div(d,mainFlag,speaker,colors);
-            }else{
-                const speaker= defaultnarrator.current;
-                const colors = [fg_colors[1],bg_colors[2]];
-                const d = `<span style="background-color:inherit;">${txt}</span>`
-                return text_div(d,true,speaker,colors)
-
-            }
-            case "\~": return `<span style="background-color:inherit; color:#afaf00">${txt}</span>`;
-            case "\*": return `<span style="background-color:inherit; color:#44aaff">${txt}</span>`;
-            case "\`": return `<code style="background-color:#4f4f4f;  color:#6a6a6a;";>${txt}</code>`;
-           
-        }
-    }
     const lines_types = [...sw,...tw,...aw,...dw];
 
     const transforms_words = lines_types.map(x=>{
@@ -293,15 +138,10 @@ const mdx_block_parser = (block,dfn)=>{
         return obj;
     })
 
-
-
-
-
-
     transforms_data = transforms_data.map(x=>{
         return {
             o:x.o,
-            t:`<pre style="background-color:#4f4f4f; width:100%; font-size:65%; white-space: pre-wrap;word-wrap: break-word;"><span style="background-color:inherit;color:#aFafd0">${x.t}</span></pre>`
+            t:`<pre class="code"><span>${x.t}</span></pre>`
         }
     })
     const transforms = [...transforms_data,...transforms_words].reduce((acc,t)=>{
@@ -321,4 +161,149 @@ const mdx_block_parser = (block,dfn)=>{
     block=block.replaceAll(/([%][%][%][%])/gm,`<br /><div style="text-align: center;color:ff7f3f">%%%%</div><br /><br />`);
     return block;
 }
-module.exports={mdx_parser,transform_to_md};
+
+function text_div(txt,mainNB,speaker,type) {
+    return`
+    <div 
+        class=" bubble ${mainNB?"narrator":"other"} ${mainNB?type:`${type}2`}">
+        <div class="bubble_label">${speaker}</div>
+        <div class="type"" style="padding-${mainNB?'left':'right'}: 15;">${txt}</div>
+        </div>`
+}
+function scp(txt,internal_data_flag){
+    const speaker_regex = txt.match(/\w+[:][:]/gm);
+    const emoji_regex = txt.match(/[$][E][\w]*[:]/gm);
+    if(emoji_regex){
+        for(const match of emoji_regex){
+        let key = match.slice(2,emoji_regex.indexOf(":"));
+        let emoji = emoji_map[key];
+        txt=txt.replace(match, emoji);
+        }
+    }
+
+    switch(txt[0]){
+        case "\"": 
+            if(internal_data_flag){
+                return `<span class="speech3">${txt}</span>`;
+            }
+            if(speaker_regex){
+                let key = speaker_regex[0].slice(0,speaker_regex.indexOf(":")-1);
+                const speaker = get_speaker_with_title(key);
+                const mainFlag = speaker== defaultnarrator.current;
+                const dialouge = txt[0]+txt.slice(txt.indexOf("::")+2)
+                const d = `<span>${dialouge}</span>`
+                return text_div(d,mainFlag,speaker,"speech");
+            }
+            return `<span>${txt}</span>`;
+        case "\'": 
+        if(internal_data_flag){
+            return `<span class="thought3">${txt}</span>`;
+        }
+        if(speaker_regex){
+            let key = speaker_regex[0].slice(0,speaker_regex.indexOf(":")-1);
+                const speaker = get_speaker_with_title(key);
+                const mainFlag = speaker== defaultnarrator.current;
+                const dialouge = txt[0]+txt.slice(txt.indexOf("::")+2)
+                const d = `<span>${dialouge}</span>`
+                return text_div(d,mainFlag,speaker,"thought");
+        }else{
+            const speaker= defaultnarrator.current;
+            const d = `<span>${txt}</span>`
+            return text_div(d,true,speaker,"thought")
+
+        }
+        case "\~": return `<span style="color:#afaf00">${txt}</span>`;
+        case "\*": return `<span style="color:#44aaff">${txt}</span>`;
+        case "\`": return `<code class="code";>${txt}</code>`;
+       
+    }
+}
+async function transform_to_md(cwd,callback){
+    var re = /\[(.*?)\]/g
+    var re2 = /\((.*?)\)/g
+    var re3 = /\{(.*?)\}/g
+    return  readFile(cwd,"utf-8",(err,data)=>{
+        if(data==undefined||err){
+            console.log("got this - ",data,err,cwd);
+            callback("<h1>there is no file here</h1>",null);
+            return;
+        }
+        const links = data?.match(/[^!](\[(.*?)\]\(.*?\))/g) ||[]
+        const images = data?.match(/[!](\[(.*?)\]\(.*?\)\{?.*?\})/g) ||[]
+        const headers = data?.match(/((?<atxlayer>#+)\s*(?<atxname>.+))|((?<setexname>[\w|\d|\s|-]+)\n(?<setexLayer>[-|=]{2,}))/g)||[]; // matches lines with # at start
+
+
+        headers.forEach(head=>{
+            const count = head.match(/^#+/)[0].length;
+            const htext = head.replace(/^#+/,"");
+            const replace = `<h${count} style="text-align: center;">${htext}</h${count}>`
+            data=data.replace(head,replace);
+        })
+
+        links.forEach(link=>{
+            const m1 = link.match(re2)[0].replace("(","").replace(")","");
+            const m2 = link.match(re)[0].replace("]","").replace("[","");
+            const replace = `<a href="${m1}">${m2}</a>`
+            data=data.replace(link,replace)
+        });
+        images.forEach(img=>{
+            
+            if(cwd.includes("/public/index.md")){
+
+            }
+            const urlandhover = img.match(re2)[0].replace("(","").replace(")","");
+            const style = img.match(re3)[0]?.replace("{","")?.replace("}","");
+            const url = urlandhover.slice(0,urlandhover.indexOf(" "))
+            const altText = img.match(re)[0].replace("]","").replace("[","");
+            
+            const replace = `<img src="${url}" alt="${altText}" style="${style}"/>`
+            data=data.replace(img,replace)
+        })
+        const script_bypass={data:""}
+        const script = data.match(/<script[^>]*>[\s\S]*?<\/script>/gim)
+        script_bypass.data=script?script[0]:"";``
+        data=data.replaceAll(script,"");
+        const blocks =mdx_parser(data)
+        if(blocks){
+
+            const start=data.indexOf("{");
+            const end = data.lastIndexOf("}");
+            const start_str = data.slice(0,start);
+            const end_str = data.slice(end+1);
+            data= start_str+blocks+end_str
+        }else{
+            const lines = data.split(EOL).filter(x=>x!=''&&x[0]!="{"&& !(x[1]=='h'&&x[0]=="<"));
+        
+            lines.forEach((line,i,arr)=>{
+                if('123456789'.includes(line[0])){
+                    const stripped_line = line.replace(line.match(/[0-9]+\.\s/gm),"")
+                    const data_line = stripped_line.slice(stripped_line.indexOf(".")+1);
+                    if(line[0]=="1"&&line[1]=="."){
+
+                        data=data.replace(line,`<ol style="font-size:5vh; margin-left:2vw" ><li >${data_line}</li>`)
+                    }
+                    else if(!arr[i+1] || !'123456789'.includes(arr[i+1][0])){
+                        data=data.replace(line,`<li>${data_line}</li></ol>`)
+                    }else{
+                    data=data.replace(line,`<li>${data_line}</li>`)
+                    }
+                }else{
+
+                    data=data.replace(line, `<p style="font-size:5vh;">${line}</p>`);
+                }
+            })
+        }
+        data=`<html><meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <title>Namshub Et'Tulpa</title>
+        <link rel="stylesheet" href="style.css">
+        <body>
+        ${data}
+        </body>
+        ${script_bypass.data}
+        </html>`
+        data=data.replace(/([\<][\p][\>][\<][\/][\p][\>])/gm, "")
+        callback(null,data);
+    })        
+}
+module.exports=transform_to_md;
